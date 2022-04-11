b406346c14cc0831c939187bf1f6a4864ecff873
Daniel Tenner <daniel.github@tenner.org>
2022-04-11 12:54:31 +0000

    New post

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+cd /home/dtenner/swombat.io
 bundle exec jekyll build
 cp -R _site/* ../swombat.github.io/
 cd ../swombat.github.io && git add * && git commit -m "New post" && git push
